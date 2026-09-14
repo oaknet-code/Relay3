b03e9e997cbc58be8414cdb7e4aa353597182d6b
oaknet-code <bashir@oaknetbusiness.com>
2026-09-14 15:14:49 +0300

    Fixed critical vulnerability

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -34,7 +34,6 @@ exports.login = async (req, res) => {
       token,
       user: {
         id: user._id,
-        email: user.email,
         firstName: user.firstName,
         lastName: user.lastName,
         role: user.role,
